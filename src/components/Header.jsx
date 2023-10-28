0e1276c878e5ef20ff733a64e00084d322fda852
Gbolahan2003 <matthewarowosegbe4@gmail.com>
2023-10-28 19:06:37 +0100

    header-component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import '../styles/header.scss'
-
+import coffeimage from '../images/coffeelogo.png'
+import background from '../images/coffeeBackground.png'
 const Header = () => {
   return (
-    <div></div>
+    <section>
+      <div className="header-container">
+        <div className="header-logo"><img src={coffeimage} alt="" /></div>
+        <div className="header-text">
+          <div className="header-head">Coffee Makes Mood</div>
+          <div className="secondary-header">"indulge in every sip: Experience the Richness of Coffee!"</div>
+
+        </div>
+        <div className="header-image"><img className='background' src={background} alt="" /></div>
+      </div>
+    </section>
   )
 }
 
