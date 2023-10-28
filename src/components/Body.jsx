0fedeb2480935c7bb855c18a043b39a70b2966f6
Gbolahan2003 <matthewarowosegbe4@gmail.com>
2023-10-28 19:26:00 +0100

    body-commit

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
-
+import '../styles/body.scss'
+import image from '../images/coffeeImage.png'
 export const Body = () => {
   return (
-    <div>Body</div>
+    <div className='body-container'>
+      <div className="body-container-image">
+        <img src={image} alt="" />
+      </div>
+      <div className="body-text">
+        <div className="body-header">Discover Coffee Borocelle</div>
+        <div className="body-secondary-header">Elevating Your Coffee Experience</div>
+        <div className="text-1">At Coffee Borcelle, we're on a mission to redefine your coffee moments. Our carefully curated selection of premium beans, sourced from the world's most renowned coffee regions.
+</div>
+        <div className="text-1">
+          
+With expert craftsmanship and a commitment to quality, 
+Join us in savoring the art of coffee, one cup at a time.
+        </div>
+        <div className="button-container">
+          <button>Read More</button>
+        </div>
+      </div>
+    </div>
   )
 }
